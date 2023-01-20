964b7b195dfa5f4df06db2b852934d24f534f472
Alex W <amwiederman15@gmail.com>
2023-01-20 18:59:44 -0500

    need to move all inquirer to index page, trying to get classes and inheritence to work

--- index.js
+++ index.js
@@ -9,7 +9,77 @@ const employee = new Employee();
 const Manager = employee.getName();
 
 
+// inquirer
+        //     .prompt([
+        //         {
+        //             type: 'input',
+        //             name: 'name',
+        //             message: "Enter name:",
+        //         }
+        //     ])
+        //     .then((data) => {
+        //         this.name = data.name;
+        //         this.getId();
+        //     })
 
+        // inquirer
+        //     .prompt([
+        //         {
+        //             type: 'input',
+        //             name: 'github',
+        //             message: "Enter Github Username:",
+        //         }
+        //     ])
+        //     .then((data) => {
+        //         this.github = data.github;
+        //         employee.getName()
+        //     })
+
+        // inquirer
+        //     .prompt([
+        //         {
+        //             type: 'input',
+        //             name: 'id',
+        //             message: "Enter id number:",
+        //         }
+        //     ])
+        //     .then((data) => {
+        //         this.id = data.id;
+        //         this.getEmail()
+        //     })
+
+        // inquirer
+            //     .prompt([
+            //         {
+            //             type: 'list',
+            //             name: 'questionRole',
+            //             message: 'Do you want to add another team member?',
+            //             choices: ["Add Intern", "Add Engineer", "No, I am done"]
+            //         }
+            //     ])
+            //     .then((data) => {
+            //         console.log(data)
+            //         if (data.questionRole == 'Add Intern') {
+            //             Intern.getSchool();
+            //         } else if (data.questionRole == 'Add Engineer') {
+            //             Engineer.getGitHub();
+            //         } else if(data.questionRole == 'No, I am done') {
+            //             console.log('Need to create HTML File')
+            //         }
+            //     })
+
+            // inquirer
+        //     .prompt([
+        //         {
+        //             type: 'input',
+        //             name: 'school',
+        //             message: "Enter school:",
+        //         }
+        //     ])
+        //     .then((data) => {
+        //         this.school = data.school;
+        //         employee.getName()
+        //     })
 
 // function createFiles() {
 
