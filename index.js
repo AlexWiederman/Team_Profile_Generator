28498d25030b969b1b4f7ba359c0c9dd2b94ee29
Alex W <amwiederman15@gmail.com>
2023-01-21 09:41:37 -0500

    Trying to put variable cards into HTML doc

--- index.js
+++ index.js
@@ -1,38 +1,108 @@
 // Include packages needed for this application
+
 const fs = require('fs'); //for creating html and css docs
-const Employee = require("./lib/Employee")
+const inquirer = require('inquirer'); //for comand prompt questions
+const Employee = require("./lib/Employee");
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager')
+const Intern = require('./lib/Intern')
+var mCard
 
-// Initialize objects in other files
+function manager() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "Enter name:",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Enter id number:",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter email address:",
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: "Enter office number:",
+            }
 
-const employee = new Employee();
+        ])
+        .then((data) => {
+            new Manager(data.name, data.id, data.email, data.officeNumber);
+            mCard = managerCard(data);
 
-const Manager = employee.getName();
 
-class Memember {
-
+            loopQuestions()
+        })
 }
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: "Enter name:",
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: "Enter id number:",
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "Enter email address:",
-        },
 
-    ])
-    .then((data) => {
+function engineer() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "Enter name:",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Enter id number:",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter email address:",
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "Enter Github Username:",
+            }
 
-    })
+        ])
+        .then((data) => {
+            new Engineer(data.name, data.id, data.email, data.github);
+            loopQuestions()
+        })
+}
+
+function intern() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "Enter name:",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Enter id number:",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter email address:",
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "Enter school:",
+            }
+
+        ])
+        .then((data) => {
+            new Intern(data.name, data.id, data.email, data.school);
+            loopQuestions()
+        })
+}
 
 function loopQuestions() {
     inquirer
@@ -45,49 +115,62 @@ function loopQuestions() {
             }
         ])
         .then((data) => {
-        })
-}
-
-
-function github() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'github',
-                message: "Enter Github Username:",
+            if (data.questionRole == "Add Intern") {
+                intern()
+            } else if (data.questionRole == "Add Engineer") {
+                engineer()
+            } else {
+                // createFiles()
+                console.log(mCard)
+                // const file = HTML(mCard);
+                fs.writeFile('./dist/website.html', file, (err) =>
+                    err ? console.log(err) : console.log('Successfully created HTML File!')
+                );
             }
-        ])
-        .then((data) => {
         })
 }
 
-function github() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'school',
-                message: "Enter school:",
-            }
-        ])
-        .then((data) => {
-        })
+//Starting with manager
+manager();
+
+HTML = ({ mCard }) =>
+    `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Team Profile</title>
+</head>
+<body>
+ ${mCard}
+</body>
+</html>`;
+
+
+const managerCard = ({ name, id, email, officeNumber }) =>
+    `
+    <div class="card">
+    <div class="card-body">
+      ${name}
+      ${id}
+      ${email}
+      ${officeNumber}
+    </div>
+  </div>`;
+
+
+function createFiles() {
+
+    // Create HTML file
+
+    // fs.writeFile('../dist/website.html', HTMLFile, (err) =>
+    //     err ? console.log(err) : console.log('Successfully created HTML File!')
+    // );
+    // Create CSS
+    // CSSFile = generateCSS(data)
+    // fs.writeFile('../dist/style.css', CSSFile, (err) =>
+    //     err ? console.log(err) : console.log('Successfully created CSS File!')
+    // );
 }
-
-
-
-// function createFiles() {
-
-//     // Create HTML data from data above
-//     HTMLFile = generateHTML(data)
-//     fs.writeFile('../dist/website.html', HTMLFile, (err) =>
-//         err ? console.log(err) : console.log('Successfully created HTML File!')
-//     );
-//     // Create CSS
-//     CSSFile = generateCSS(data)
-//     fs.writeFile('../dist/style.css', CSSFile, (err) =>
-//         err ? console.log(err) : console.log('Successfully created CSS File!')
-//     );
-// }
 
