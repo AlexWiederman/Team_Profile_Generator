5f85cd83896b6510e0f11c8c1a365341a5fed774
Alex W <amwiederman15@gmail.com>
2023-01-21 08:08:11 -0500

    put all questions in index. still need to call classes to create them from questions

--- index.js
+++ index.js
@@ -8,78 +8,74 @@ const employee = new Employee();
 
 const Manager = employee.getName();
 
+class Memember {
 
-// inquirer
-        //     .prompt([
-        //         {
-        //             type: 'input',
-        //             name: 'name',
-        //             message: "Enter name:",
-        //         }
-        //     ])
-        //     .then((data) => {
-        //         this.name = data.name;
-        //         this.getId();
-        //     })
+}
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter name:",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Enter id number:",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Enter email address:",
+        },
 
-        // inquirer
-        //     .prompt([
-        //         {
-        //             type: 'input',
-        //             name: 'github',
-        //             message: "Enter Github Username:",
-        //         }
-        //     ])
-        //     .then((data) => {
-        //         this.github = data.github;
-        //         employee.getName()
-        //     })
+    ])
+    .then((data) => {
 
-        // inquirer
-        //     .prompt([
-        //         {
-        //             type: 'input',
-        //             name: 'id',
-        //             message: "Enter id number:",
-        //         }
-        //     ])
-        //     .then((data) => {
-        //         this.id = data.id;
-        //         this.getEmail()
-        //     })
+    })
 
-        // inquirer
-            //     .prompt([
-            //         {
-            //             type: 'list',
-            //             name: 'questionRole',
-            //             message: 'Do you want to add another team member?',
-            //             choices: ["Add Intern", "Add Engineer", "No, I am done"]
-            //         }
-            //     ])
-            //     .then((data) => {
-            //         console.log(data)
-            //         if (data.questionRole == 'Add Intern') {
-            //             Intern.getSchool();
-            //         } else if (data.questionRole == 'Add Engineer') {
-            //             Engineer.getGitHub();
-            //         } else if(data.questionRole == 'No, I am done') {
-            //             console.log('Need to create HTML File')
-            //         }
-            //     })
+function loopQuestions() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'questionRole',
+                message: 'Do you want to add another team member?',
+                choices: ["Add Intern", "Add Engineer", "No, I am done"]
+            }
+        ])
+        .then((data) => {
+        })
+}
 
-            // inquirer
-        //     .prompt([
-        //         {
-        //             type: 'input',
-        //             name: 'school',
-        //             message: "Enter school:",
-        //         }
-        //     ])
-        //     .then((data) => {
-        //         this.school = data.school;
-        //         employee.getName()
-        //     })
+
+function github() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'github',
+                message: "Enter Github Username:",
+            }
+        ])
+        .then((data) => {
+        })
+}
+
+function github() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'school',
+                message: "Enter school:",
+            }
+        ])
+        .then((data) => {
+        })
+}
+
+
 
 // function createFiles() {
 
@@ -88,7 +84,7 @@ const Manager = employee.getName();
 //     fs.writeFile('../dist/website.html', HTMLFile, (err) =>
 //         err ? console.log(err) : console.log('Successfully created HTML File!')
 //     );
-//     // Create CSS 
+//     // Create CSS
 //     CSSFile = generateCSS(data)
 //     fs.writeFile('../dist/style.css', CSSFile, (err) =>
 //         err ? console.log(err) : console.log('Successfully created CSS File!')
